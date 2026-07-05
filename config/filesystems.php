2ad31fdb04e36006f5451f372c7800e8be873a93
ismailasinan-cmyk <ismailasinan@gmail.com>
2026-07-05 18:14:36 +0100

    Allow defining specific Cloudflare R2 region for EU endpoints

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,7 +64,7 @@ return [
             'driver' => 's3',
             'key' => env('CLOUDFLARE_R2_ACCESS_KEY_ID'),
             'secret' => env('CLOUDFLARE_R2_SECRET_ACCESS_KEY'),
-            'region' => 'auto',
+            'region' => env('CLOUDFLARE_R2_REGION', 'auto'),
             'bucket' => env('CLOUDFLARE_R2_BUCKET'),
             'endpoint' => env('CLOUDFLARE_R2_ENDPOINT'),
             'use_path_style_endpoint' => true,
